daa0274bf1b0ff8996e1258f3e242da575636deb
unknown <steggihalli@gmail.com>
2022-05-17 09:13:44 +0000

    my first java prg

---
+++ 1 day/Adding.java
@@ -0,0 +1,12 @@
+package com;
+
+public class Adding {
+	public static void main(String[] args) {
+		int a = 20;
+		int b = 30;
+		int sum = a+b;
+		
+		System.out.println("The sum of "+a+" and "+b+" is "+sum);
+		
+	}
+}
